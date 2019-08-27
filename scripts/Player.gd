4eb05ac1c97437c998379433bdf0d33a03d0a62f
peternatewood <peternatewood@tutanota.com>
2019-08-27 15:32:10 -0400

    Update player to accelerate and brake

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,22 +3,29 @@ extends Area
 onready var camera: Camera = get_viewport().get_camera()
 onready var scene_controller = get_node("/root/Scene")
 
-var input_velocity: Vector2
-var velocity: Vector3
+var input_velocity: Vector3
+var velocity: Vector3 = Vector3(0, 0, -BASE_SPEED)
 
 
 func _process(delta):
 	input_velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_velocity.y = Input.get_action_strength("up") - Input.get_action_strength("down")
 
-	velocity.x = lerp(velocity.x, input_velocity.x, TURN_SPEED * delta)
-	velocity.y = lerp(velocity.y, input_velocity.y, TURN_SPEED * delta)
+	velocity.x = lerp(velocity.x, MOVEMENT_SPEED * input_velocity.x, TURN_SPEED * delta)
+	velocity.y = lerp(velocity.y, MOVEMENT_SPEED * input_velocity.y, TURN_SPEED * delta)
 
-	rotation.x = velocity.y
-	rotation.y = -velocity.x
-	rotation.z = -velocity.x
+	rotation.x = TURN_MOD * velocity.y
+	rotation.y = TURN_MOD * -velocity.x
+	rotation.z = TURN_MOD * -velocity.x
 
-	global_translate(velocity)
+	if Input.is_action_pressed("accelerate"):
+		velocity.z = lerp(velocity.z, -MAX_SPEED, ACCELERATION * delta)
+	elif Input.is_action_pressed("brake"):
+		velocity.z = lerp(velocity.z, -MIN_SPEED, ACCELERATION * delta)
+	else:
+		velocity.z = lerp(velocity.z, -BASE_SPEED, ACCELERATION * delta)
+
+	global_translate(delta * velocity)
 
 	# Handle boundary collisions
 	transform.origin = scene_controller.handle_boundary_collision(transform.origin)
@@ -28,6 +35,12 @@ func _process(delta):
 	camera.look_at(transform.origin - transform.basis.z, Vector3.UP)
 
 
+const ACCELERATION: float = 5.0
+const BASE_SPEED: float = 40.0
 const CAM_OFFSET: Vector3 = 2 * Vector3.UP + 12 * Vector3.BACK
 const CAM_OFFSET_PERCENT: float = 0.95
+const MAX_SPEED: float = 80.0
+const MIN_SPEED: float = 6.0
+const MOVEMENT_SPEED: float = 64.0
+const TURN_MOD: float = 0.5 / MOVEMENT_SPEED
 const TURN_SPEED: float = 2.0
